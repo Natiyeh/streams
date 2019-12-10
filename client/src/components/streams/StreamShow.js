c05361c1eac62da4c1ca5b5e431f04fc3c7fcb98
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-12-10 16:10:48 -0600

    added video element

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
-
   constructor(props) {
+    super(props);
 
+    this.videoRef = React.createRef();
   }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
   }
@@ -21,7 +23,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
-        <video />
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
